41c1b348bf66d56e8b3378aaaf4f92aeb44270cd
asrilaizal <asrilaizal@gmail.com>
2022-06-28 23:29:53 +0800

    Updates

---
+++ precache-manifest.32edba629f321aea264bc04786f0e6da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b2e186186d8fe06476cc1abc964981",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e842b121caa3086c4e78",
+    "url": "/portfolio/static/css/main.ea8e5f11.chunk.css"
+  },
+  {
+    "revision": "ef02ea6d01ef814393ad",
+    "url": "/portfolio/static/js/2.b06002cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.b06002cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e842b121caa3086c4e78",
+    "url": "/portfolio/static/js/main.ff71d296.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "456d27d30afacf5ef9c2d04e25452c99",
+    "url": "/portfolio/static/media/mock1.456d27d3.png"
+  },
+  {
+    "revision": "37e11540fe7357463bd40d3effb35da1",
+    "url": "/portfolio/static/media/mock2.37e11540.png"
+  },
+  {
+    "revision": "6a736fcd570ebcf9699f8d5e9f949f60",
+    "url": "/portfolio/static/media/mock3.6a736fcd.png"
+  },
+  {
+    "revision": "bd6a383d15853619853b9eba50524095",
+    "url": "/portfolio/static/media/mock4.bd6a383d.png"
+  },
+  {
+    "revision": "aa1917bdce8260a1fb231e1c6c0953cf",
+    "url": "/portfolio/static/media/self.aa1917bd.png"
+  }
+]);
